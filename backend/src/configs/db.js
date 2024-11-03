fe2cb16d1fc1b24482747d0ee5e32f2af0dc7dbb
ThuatVDQ <thuatvopro@gmail.com>
2024-11-03 17:37:19 +0700

    connect db

---
+++ backend/src/configs/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    //const uri = "mongodb://localhost:27017/ten_database";
+    const uri = process.env.MONGODB_URI;
+    console.log(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
-const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
+const connectDB = require("./configs/db");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
